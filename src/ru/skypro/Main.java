8f209a240a4dba24e7c93376ca8d141a2e5b32dc
VIKI <anystrelchenko30@gmail.com>
2022-04-22 21:43:34 +0300

    Доплнительное задание №8

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,7 +3,7 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// Задание 8
+	// Задание №8
         int a=514;
         int b=a/10%10;
         System.out.println(b);
