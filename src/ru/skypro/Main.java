d3702d2727064256790a2e472bf27cb238b54a60
VIKI <anystrelchenko30@gmail.com>
2022-04-22 21:22:56 +0300

    Доплнительное задание №6

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,16 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задача 6
+        int a=12;
+        int b=27;
+        int c=44;
+        int d=15;
+        int e=9;
+        int result=a*(b+(c-d*e));
+        System.out.println(result);
+        int newResult=Math.abs(result);
+        System.out.println(newResult);
+
     }
 }
